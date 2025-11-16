f3a7fffe21d9acf2b6f808f81b3428f20addeeae
Fabio Rodrigo Ortiz dos Santos <113561869+fbrtz@users.noreply.github.com>
2025-11-16 05:41:43 -0300

    Delete AlbergueAlmeida directory

--- AlbergueAlmeida/lado_admin/gerenciar_vagas/api/excluir_vaga.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = new mysqli("localhost","root","","albergue_almeida");
-$id = isset($_POST['id']) ? intval($_POST['id']) : 0;
-if ($id <= 0) { http_response_code(400); echo "ERR"; exit; }
-$con->query("DELETE FROM vagas WHERE id = $id");
-echo "OK";
--- AlbergueAlmeida/lado_admin/gerenciar_vagas/api/vincular_caracteristica.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = new mysqli("localhost","root","","albergue_almeida");
-$vaga_id = intval($_POST['vaga_id'] ?? 0);
-$carac_id = intval($_POST['caracteristica_id'] ?? 0);
-if ($vaga_id <=0 || $carac_id<=0) { http_response_code(400); echo "ERR"; exit; }
-$con->query("INSERT IGNORE INTO caracteristicas_vagas (vaga_id, caracteristica_id) VALUES ($vaga_id, $carac_id)");
-echo "OK";
